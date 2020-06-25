12af7b19949f2aed7ad989677660f15a60e46032
sengeiii <62093338+sengeiii@users.noreply.github.com>
2020-06-25 12:52:32 +0800

    Add files via upload
    2.0

--- script.js
+++ script.js
@@ -17,44 +17,30 @@ function rendreEditor() {
             title: inputEl.value,
             done: false,
         };
-
         inputEl.value = "";
-
         tasks.push(newTask);
-
         console.log("tasks: ", tasks);
-
         renderTaskItems();
     };
-
     inputEl.onkeypress = (e) => {
-
         if (e.key == "Enter") {
             addTask();
         }
     }
-
     let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
     addEl.onclick = (e) => {
         addTask();
-
     };
-
 }
-
 function renderTaskItems() {
     console.log("render items");
     let itemsEl = document.querySelector("#default-todo-panel .todo-items");
-
     itemsEl.querySelectorAll("div").forEach((node) => node.remove());
-
     console.log(itemsEl);
-
     for (let i = 0; i < tasks.length; i++) {
         let task = tasks[i];
         let itemEl = document.createElement("div");
         itemEl.className = "task";
-
         let doneEl = document.createElement("input");
         doneEl.type = "checkbox";
         doneEl.checked = task.done;
@@ -63,7 +49,6 @@ function renderTaskItems() {
         } else {
             itemEl.classList.remove("done");
         }
-
         doneEl.onchange = (e) => {
             task.done = e.target.checked;
             // console.log("checkbox: ", e);
@@ -75,23 +60,17 @@ function renderTaskItems() {
             }
         }
         itemEl.append(doneEl);
-
         let titleEl = document.createElement("label");
         titleEl.innerText = task.title;
         itemEl.append(titleEl);
-
         let ctrlbarEl = renderTaskCtrlbar(tasks, i);
-
         itemEl.append(ctrlbarEl);
-
         itemsEl.append(itemEl);
     }
 }
-
 function renderTaskCtrlbar(tasks, taskIdx) {
     let ctrlbarEl = document.createElement("div");
     ctrlbarEl.className = "ctrlbar";
-
     let upEl = document.createElement("button");
     if (taskIdx === 0) {
         upEl.disabled = true;
@@ -103,7 +82,6 @@ function renderTaskCtrlbar(tasks, taskIdx) {
         tasks[taskIdx-1]=t;
         renderTaskItems();
     }
-    
     ctrlbarEl.append(upEl);
     let downEl = document.createElement("button");
     downEl.innerText = "↓";
@@ -116,9 +94,7 @@ function renderTaskCtrlbar(tasks, taskIdx) {
     if (taskIdx === 0) {
         downEl.disabled = true;
     }
-    
     ctrlbarEl.append(downEl);
-
     let signEl = document.createElement("button");
     signEl.innerText = "★";
     ctrlbarEl.append(signEl);
